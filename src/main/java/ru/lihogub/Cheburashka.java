4d5b8681136e8ecd0caa72b53c41f8ee2edf30d6
Lihogub Oleg <oligli@yandex.ru>
2021-09-05 11:43:03 +0300

    class 2 step (constructor)

--- src/main/java/ru/lihogub/Cheburashka.java
+++ src/main/java/ru/lihogub/Cheburashka.java
@@ -2,4 +2,9 @@ package ru.lihogub;
 
 public class Cheburashka {
     String cheburashkaSound; // это поле
+
+    /* это конструктор */
+    Cheburashka(String sound) {
+        cheburashkaSound = sound;
+    }
 }
