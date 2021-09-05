54dabbfb6f1fd7cbb47a0e8c204bc17c94559fd5
Lihogub Oleg <oligli@yandex.ru>
2021-09-05 11:58:43 +0300

    instantiation of single Cheburashka object

--- src/main/java/ru/lihogub/Main.java
+++ src/main/java/ru/lihogub/Main.java
@@ -2,13 +2,13 @@ package ru.lihogub;
 
 public class Main {
     public static void main(String[] args) {
-        byte byteVar = 0b101;
-        short shortVar = 123;
-        int intVar = 30000;
-        long longVar = 3000000L;
-        float floatVar = 1.6e-7f;
-        double doubleVar = 1.65e+15;
-        char charVar = 'G';
-        boolean booleanVar = true;
+        // создали ссылку (в термина C++ - это на самом деле указатель)
+        Cheburashka cheburashka;
+
+        // создаем объект в куче и ссылку на него кладем в переменную cheburashka
+        cheburashka = new Cheburashka("}{очу апельсинов!");
+
+        /* вызываем метод */
+        cheburashka.makeSound();
     }
 }
