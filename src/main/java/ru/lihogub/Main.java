9c2eb6db001fe544db7ba51246df306f36684a18
Lihogub Oleg <oligli@yandex.ru>
2021-09-05 11:33:51 +0300

    sum

--- src/main/java/ru/lihogub/Main.java
+++ src/main/java/ru/lihogub/Main.java
@@ -2,6 +2,12 @@ package ru.lihogub;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        int sum = 0;
+
+        for (int i = 0; i < 5; i++) {
+            sum += i;
+        }
+
+        System.out.println(sum);
     }
 }
