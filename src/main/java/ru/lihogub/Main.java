6d1a3dfcabb0202df87d4998752a1ea8210fa403
Lihogub Oleg <oligli@yandex.ru>
2021-09-05 11:37:15 +0300

    data types

--- src/main/java/ru/lihogub/Main.java
+++ src/main/java/ru/lihogub/Main.java
@@ -2,12 +2,13 @@ package ru.lihogub;
 
 public class Main {
     public static void main(String[] args) {
-        int sum = 0;
-
-        for (int i = 0; i < 5; i++) {
-            sum += i;
-        }
-
-        System.out.println(sum);
+        byte byteVar = 0b101;
+        short shortVar = 123;
+        int intVar = 30000;
+        long longVar = 3000000L;
+        float floatVar = 1.6e-7f;
+        double doubleVar = 1.65e+15;
+        char charVar = 'G';
+        boolean booleanVar = true;
     }
 }
